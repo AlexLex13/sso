ef1cf99be7b4f78818ace9d99a9ed8bc1b1cf27b
Alexey <suxovarov.alexey@gmail.com>
2023-12-07 13:27:24 +0300

    Add services

---
+++ internal/services/auth/auth.go
@@ -0,0 +1,19 @@
+package auth
+
+import (
+	"context"
+
+	"github.com/AlexLex13/sso/internal/domain/models"
+)
+
+type UserSaver interface {
+	SaveUser(
+		ctx context.Context,
+		email string,
+		passHash []byte,
+	) (uid int64, err error)
+}
+
+type UserProvider interface {
+	User(ctx context.Context, email string) (models.User, error)
+}
